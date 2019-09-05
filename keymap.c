66fe812aceeb7e35bca35eb4443940c76d341de3
David Kristoffersen <david.alta2010@gmail.com>
2019-09-05 22:30:42 +0200

    Try to include default keymap overhead in prog_qgmlwb keymap

--- keymap.c
+++ keymap.c
@@ -1,4 +1,37 @@
+/*
+	Pre default keymap.c
+*/
+
 #include QMK_KEYBOARD_H
+#include "muse.h"
+
+extern keymap_config_t keymap_config;
+
+enum planck_layers {
+	_QWERTY,
+	_COLEMAK,
+	_DVORAK,
+	_LOWER,
+	_RAISE,
+	_PLOVER,
+	_ADJUST
+};
+
+enum planck_keycodes {
+	QWERTY = SAFE_RANGE,
+	COLEMAK,
+	DVORAK,
+	PLOVER,
+	BACKLIT,
+	EXT_PLV
+};
+
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+
+/*
+	Layouts imported from QMK Configurator
+*/
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_ortho_4x12(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI, MO(3), KC_SPC, KC_SPC, MO(4), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
@@ -18,3 +51,177 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[14] = LAYOUT_ortho_4x12(LSFT(KC_TAB), LSFT(KC_Q), LSFT(KC_G), LSFT(KC_M), LSFT(KC_L), LSFT(KC_W), LSFT(KC_B), LSFT(KC_Y), LSFT(KC_U), LSFT(KC_V), KC_AT, KC_BSPC, KC_ESC, LSFT(KC_D), LSFT(KC_S), LSFT(KC_T), LSFT(KC_N), LSFT(KC_R), LSFT(KC_I), LSFT(KC_A), LSFT(KC_E), LSFT(KC_O), LSFT(KC_H), LSFT(KC_ENT), KC_UNDS, LSFT(KC_Z), LSFT(KC_X), LSFT(KC_C), LSFT(KC_F), LSFT(KC_J), LSFT(KC_K), LSFT(KC_P), KC_QUES, LSFT(KC_COMM), LSFT(KC_DOT), KC_GRV, KC_UNDS, KC_LCTL, KC_LALT, KC_LSFT, KC_NO, KC_SPC, KC_SPC, KC_NO, LSFT(KC_LEFT), LSFT(KC_DOWN), LSFT(KC_UP), LSFT(KC_RGHT)),
 	[15] = LAYOUT_ortho_4x12(KC_TRNS, RESET, DEBUG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL, KC_TRNS, KC_TRNS, MU_MOD, AU_ON, AU_OFF, AG_NORM, AG_SWAP, DF(0), DF(7), DF(8), DF(1), DF(2), KC_TRNS, MUV_DE, MUV_IN, MU_ON, MU_OFF, MI_ON, MI_OFF, TERM_ON, TERM_OFF, DF(5), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
+
+/*
+	Post default keymap.c
+*/
+
+#ifdef AUDIO_ENABLE
+	float plover_song[][2]		= SONG(PLOVER_SOUND);
+	float plover_gb_song[][2]	= SONG(PLOVER_GOODBYE_SOUND);
+#endif
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+	return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	switch (keycode) {
+		case QWERTY:
+			if (record->event.pressed) {
+				print("mode just switched to qwerty and this is a huge string\n");
+				set_single_persistent_default_layer(_QWERTY);
+			}
+			return false;
+			break;
+		case COLEMAK:
+			if (record->event.pressed) {
+				set_single_persistent_default_layer(_COLEMAK);
+			}
+			return false;
+			break;
+		case DVORAK:
+			if (record->event.pressed) {
+				set_single_persistent_default_layer(_DVORAK);
+			}
+			return false;
+			break;
+		case BACKLIT:
+			if (record->event.pressed) {
+				register_code(KC_RSFT);
+				#ifdef BACKLIGHT_ENABLE
+					backlight_step();
+				#endif
+				#ifdef KEYBOARD_planck_rev5
+					PORTE &= ~(1<<6);
+				#endif
+			} else {
+				unregister_code(KC_RSFT);
+				#ifdef KEYBOARD_planck_rev5
+					PORTE |= (1<<6);
+				#endif
+			}
+			return false;
+			break;
+		case PLOVER:
+			if (record->event.pressed) {
+				#ifdef AUDIO_ENABLE
+					stop_all_notes();
+					PLAY_SONG(plover_song);
+				#endif
+				layer_off(_RAISE);
+				layer_off(_LOWER);
+				layer_off(_ADJUST);
+				layer_on(_PLOVER);
+				if (!eeconfig_is_enabled()) {
+					eeconfig_init();
+				}
+				keymap_config.raw = eeconfig_read_keymap();
+				keymap_config.nkro = 1;
+				eeconfig_update_keymap(keymap_config.raw);
+			}
+			return false;
+			break;
+		case EXT_PLV:
+			if (record->event.pressed) {
+				#ifdef AUDIO_ENABLE
+					PLAY_SONG(plover_gb_song);
+				#endif
+				layer_off(_PLOVER);
+			}
+			return false;
+			break;
+	}
+	return true;
+}
+
+bool muse_mode = false;
+uint8_t last_muse_note = 0;
+uint16_t muse_counter = 0;
+uint8_t muse_offset = 70;
+uint16_t muse_tempo = 50;
+
+void encoder_update(bool clockwise) {
+	if (muse_mode) {
+		if (IS_LAYER_ON(_RAISE)) {
+			if (clockwise) {
+				muse_offset++;
+			} else {
+				muse_offset--;
+			}
+		} else {
+			if (clockwise) {
+				muse_tempo+=1;
+			} else {
+				muse_tempo-=1;
+			}
+		}
+	} else {
+		if (clockwise) {
+			#ifdef MOUSEKEY_ENABLE
+				tap_code(KC_MS_WH_DOWN);
+			#else
+				tap_code(KC_PGDN);
+			#endif
+		} else {
+			#ifdef MOUSEKEY_ENABLE
+				tap_code(KC_MS_WH_UP);
+			#else
+				tap_code(KC_PGUP);
+			#endif
+		}
+	}
+}
+
+void dip_switch_update_user(uint8_t index, bool active) {
+	switch (index) {
+		case 0:
+			if (active) {
+				#ifdef AUDIO_ENABLE
+					PLAY_SONG(plover_song);
+				#endif
+				layer_on(_ADJUST);
+			} else {
+				#ifdef AUDIO_ENABLE
+					PLAY_SONG(plover_gb_song);
+				#endif
+				layer_off(_ADJUST);
+			}
+			break;
+		case 1:
+			if (active) {
+				muse_mode = true;
+			} else {
+				muse_mode = false;
+				#ifdef AUDIO_ENABLE
+					stop_all_notes();
+				#endif
+			}
+	 }
+}
+
+void matrix_scan_user(void) {
+	#ifdef AUDIO_ENABLE
+		if (muse_mode) {
+			if (muse_counter == 0) {
+				uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+				if (muse_note != last_muse_note) {
+					stop_note(compute_freq_for_midi_note(last_muse_note));
+					play_note(compute_freq_for_midi_note(muse_note), 0xF);
+					last_muse_note = muse_note;
+				}
+			}
+			muse_counter = (muse_counter + 1) % muse_tempo;
+		}
+	#endif
+}
+
+bool music_mask_user(uint16_t keycode) {
+	switch (keycode) {
+		case RAISE:
+		case LOWER:
+			return false;
+		default:
+			return true;
+	}
+}
