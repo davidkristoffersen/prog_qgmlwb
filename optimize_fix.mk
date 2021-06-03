35c6d5a3b346d09c8aae9ed51a3fa621531ac5c7
David Kristoffersen <david.alta2010@gmail.com>
2021-06-03 16:13:00 +0200

    Add storage optimization rule file

---
+++ optimize_fix.mk
@@ -0,0 +1,6 @@
+BOOTLOADER = qmk-dfu
+BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
+CONSOLE_ENABLE = no		# Console for debug(+400)
+# LINK_TIME_OPTIMIZATION_ENABLE = yes
